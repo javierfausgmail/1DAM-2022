655941d6a1b5d8313db2182835272107cfc57540
antoniodiazsantacruz <antoniodiazsantacruz@localhost>
2022-01-19 18:50:08 +0100

    Añadido nombre y rama.

--- GitHub-Prueba/src/github/Prueba.java
+++ GitHub-Prueba/src/github/Prueba.java
@@ -3,9 +3,8 @@ package github;
 public class Prueba {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
-		
+		System.out.println("Hola, soy Antonio.");
 		
 	}
 
