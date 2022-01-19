64709b98c0e957cde61fe5a840fc1caa5397d3c7
Portátil Miguel <Porttil Miguel@LAPTOP-S7HHS421>
2022-01-19 18:50:56 +0100

    https://github.com/javierfausgmail/1DAM-2022

--- GitHub-Prueba/src/github/Prueba.java
+++ GitHub-Prueba/src/github/Prueba.java
@@ -1,11 +1,23 @@
 package github;
 
+import java.util.Scanner;
+
 public class Prueba {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		 Scanner sc = new Scanner(System.in);
+		 
+		 
 		
+		
+		 System.out.println("Hola mundo, soy MiguelCM19");
+		 System.out.println("Rama nueva de MiguelCM19 añadida");
+	 
+	 
+		 
+		 sc.close();
 		
 	}
 
